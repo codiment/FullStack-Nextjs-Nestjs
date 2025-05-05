9a136a5bbb2916576cb0b7355eea62fc57a3a167
codiment <uprovisional51@gmail.com>
2025-05-05 20:04:20 -0300

    fix 6

--- nextjs/src/app/products/[id]/edit/page.tsx
+++ nextjs/src/app/products/[id]/edit/page.tsx
@@ -1,1 +1,9 @@
-export {default} from "../../new/page"
+// @ts-nocheck
+import { ProductForm } from '../../new/product-form';
+import { getProduct } from '../../products.api';
+
+export default async function EditProductPage({ params }: { params: { id: string } }) {
+  const product = await getProduct(params.id);
+  
+  return <ProductForm product={product} />;
+}
