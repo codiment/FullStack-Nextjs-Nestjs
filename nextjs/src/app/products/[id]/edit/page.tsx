8e9f9f913c6d9d1f6de47e5e2a73b3d2e4588b7c
codiment <uprovisional51@gmail.com>
2025-05-05 19:44:00 -0300

    commit 5

--- nextjs/src/app/products/[id]/edit/page.tsx
+++ nextjs/src/app/products/[id]/edit/page.tsx
@@ -1,1 +1,14 @@
-export {default} from '../../new/page'
+import { ProductForm } from '../../new/product-form';
+import { getProduct } from '../../products.api';
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default async function EditProductPage({ params }: Props) {
+  const product = await getProduct(params.id);
+  
+  return <ProductForm product={product} />;
+}
